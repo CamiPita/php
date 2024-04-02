1271a7ed689257a13fecae735e7728b972bb540f
pitacami25@gmail.com <pitacami25@gmail.com>
2024-04-02 16:28:18 -0300

    cambios

--- formulario_datos_personales.php
+++ formulario_datos_personales.php
@@ -1,3 +1,71 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+if ($_POST){
+    $nombre=$_POST["txtNombre"];
+    $dni=$_POST["txtDni"];
+    $telefono=$_POST["txtTelefono"];
+    $edad=$_POST["txtEdad"];
+    if($nombre=="" && $dni=="" && $telefono=="" && $edad==""){
+        header("Location:resultado.php");
+          }
+}
+
 
 ?>
+
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE-edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulario de datos</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body>
+    <main class="container">
+        <div class="row">
+            <div class="col-12 pb-3">
+                <h1>Formulario de datos personales </h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-6">
+                <form action="resultado.php" method="POST">
+                    <div>
+                        <label form="txtNombre">Nombre:* </label>
+                        <input type="text" name="txtNombre" id="txtNombre" requerid class="form-control">
+                    </div>
+                    <div class="pb-3">
+                        <label form="txtDni">DNI:*</label>
+                        <input type="text" name="txtDni" id="txtDni" required class="form-control">
+                    </div>
+                    <div class="pb-3">
+                        <label form="txtTelefono">Telefono:*</label>
+                        <input type="tel" name="txtTelefono" id="txtTelefono" required class="form-control">
+
+                    </div>
+                    <div class="pb-3">
+                        <label form="txtEdad">Edad:*</label>
+                        <input type="txtEdad" name="txtEdad" id="txtEdad" required class="form-control">
+                    </div>
+                    <div class="pb-3 text-end">
+                        <button type="sublimt" name="btnIngresar" class="btn btn-primary">Enviar </button>
+
+                    </div>
+                </form>
+            </div>
+        </div>
+
+    </main>
+
+
+
+</body>
+
+</html>
---
+++ resultado.php
@@ -0,0 +1,68 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+if ($_POST){
+    $nombre=$_POST["txtNombre"];
+    $dni=$_POST["txtDni"];
+    $telefono=$_POST["txtTelefono"];
+    $edad=$_POST["txtEdad"];
+    if($nombre=="" && $dni=="" && $telefono=="" && $edad==""){
+       
+          }
+}
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE-edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulario de datos personales</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body>
+    <main class="container">
+        <div class="row">
+            <div class="col-12 pb-3">
+                <h1>Resultado de formulario</h1>
+            </div>
+
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <form action="" method="POST">
+                <table class="table table-hover border">
+                    <thead>
+                        <tr>
+                            <th>Nombre</th>
+                            <th>DNI</th>
+                            <th>Telefono</th>
+                            <th>Edad</th>
+
+                        </tr>
+                    <td><?php echo $nombre ?> </td>
+                    <td><?php echo $dni ?></td>
+                    <td><?php echo $telefono ?></td>
+                    <td><?php echo $edad ?></td>
+                    </thead>
+                    <tbody>
+                
+
+                    </tbody>
+
+                </table>
+                </form>
+            </div>
+
+        </div>
+    </main>
+
+</body>
+
+</html>
