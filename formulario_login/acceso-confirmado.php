dfa665604926169fe55bcff90685de489ff4a02e
CamiPita <pitacami25@gmail.com>
2024-08-28 12:15:44 -0300

    cambios

---
+++ formulario_login/acceso-confirmado.php
@@ -0,0 +1,42 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+?>
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE-edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>Listado</title>
+</head>
+
+<body>
+    <main class="containter">
+        <div class="row">
+            <div class="col-12 pb-3 ps-4">
+                <h1>Bienvenid@ al sistema</h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12 text-center ">
+                <p class="alert alert-primary" role="alert"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti accusantium amet ducimus quam ab omnis mollitia tempore quod dolorem, velit quia impedit provident iste delectus aspernatur. Sapiente provident molestias suscipit?
+
+                </p>
+            </div>
+            <div>
+                <div class="pb-3 text-end">
+                    <a href="index.php" type="submit" name="btnSalir" class="btn btn-primary" >Salir</a>
+
+                </div>
+            </div>
+
+    </main>
+
+</body>
+
+</html>
---
+++ formulario_login/index.php
@@ -0,0 +1,48 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE-edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>Formulario</title>
+</head>
+
+<body>
+    <main class="container">
+        <div class="row">
+            <div class="col-12 p-3">
+                <h1>Formulario</h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-6">
+                <form action="" method="$_POST">
+                    <div class="pb-3">
+                        <label form="txtUsuario">Usuario:</label>
+                        <input type="text" name="txtUsuario" id="txtUsuario" class="form-control">
+                    </div>
+                    <div class="pb-3">
+                        <label form="txtClave">Clave:</label>
+                        <input type="password" name="txtClave" id="txtClave" class="form-control">
+                    </div>
+                    <div class="pb-3">
+                        <button type="submit" name="btnEnviar" class="btn btn-primary">Enviar</button>
+                    </div>
+            </div>
+            </form>
+        </div>
+        </div>
+    </main>
+</body>
+
+</html>
