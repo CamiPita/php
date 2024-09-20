790ab2ae50183332422bbd8c7ef7200e54c54837
CamiPita <pitacami25@gmail.com>
2024-09-20 17:51:08 -0300

    cambios

--- clientes_session.php
+++ clientes_session.php
@@ -3,23 +3,59 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+//Indico que voy a utilizar variables de seccion, que cumple la función de guardar los datos que colocamos.
+session_start();
+//Y acá preguntamos ,si esta seteado. 
+
+if (isset($_SESSION["listadoClientes"])) {
+    //Si existe la variable de session listadoClientes asigno su contenido a aClientes.
+    $aClientes = $_SESSION["listadoClientes"];
+} else {
+    $aClientes = array();
+}
+
 
 if ($_POST) {
-    //Asignamos en variables,los datos que vienen del formulario.
 
-    $nombre = $_POST["txtNombre"];
-    $dni = $_POST["txtDni"];
-    $telefono = $_POST["txtTelefono"];
-    $edad = $_POST["txtEdad"];
+    //Si apretamos el botón ENVIAR, hace todo esto:
+    if (isset($_POST["btnEnviar"])) {
+        //Asignamos en variables,los datos que vienen del formulario.
 
-    //Creamos un array de los listados de clientes.
-    $aClientes[] = array(
-        "nombre" => $nombre,
-        "dni" => $dni,
-        "telefono" => $telefono,
-        "edad" => $edad
-    );
+        $nombre = $_POST["txtNombre"];
+        $dni = $_POST["txtDni"];
+        $telefono = $_POST["txtTelefono"];
+        $edad = $_POST["txtEdad"];
+
+        //Creamos un array de los listados de clientes.
+        $aClientes[] = array(
+            "nombre" => $nombre,
+            "dni" => $dni,
+            "telefono" => $telefono,
+            "edad" => $edad
+        );
+
+
+        //Con esto actualiza los contenidos de variables de session.
+        $_SESSION["listadoClientes"] = $aClientes;
+    }
+
+    //Si apretamos el botón ELIMINAR , elimina todo:
+    if (isset($_POST["btnEliminar"])) {
+        session_destroy();
+        $aClientes = array();
+    }
 }
+
+//Para eliminar fila por fila. Comienza aca el metodo get, lo que se ve en la query strig
+
+if (isset($_GET["pos"])) {
+    $pos = $_GET["pos"]; //Aca indica que la variable pos es igual a lo que esta en el metodo pos.
+    unset($aClientes[$pos]);
+    //Actualizo la variable de session con el array actualizado.
+    $_SESSION["listadoClientes"] = $aClientes;
+    header("Location: clientes_session.php"); //Con esto lo redirecciono a la pagina para que vuelva a estar limpia.
+}
+
 
 ?>
 
@@ -29,9 +65,11 @@ if ($_POST) {
 
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE-edge">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+
     <title>Clientes</title>
 </head>
 
@@ -44,27 +82,27 @@ if ($_POST) {
         </div>
         <div class="row">
             <div class="col-3">
-                <form action="" method="post">
+                <form action="" method="post" class="form">
                     <div class="p-2">
-                        <label form="">Nombre:</label>
+                        <label form="txtNombre">Nombre:</label>
                         <input type="text" name="txtNombre" id="txtNombre" placeholder="ingrese el nombre y apellido" class="form-control">
                     </div>
                     <div class="p-2">
-                        <label form="">DNI:</label>
+                        <label form="txtDni">DNI:</label>
                         <input type="text" name="txtDni" id="txtDni" class="form-control">
                     </div>
                     <div class="p-2">
-                        <label form="">Teléfono:</label>
+                        <label form="txtTelefono">Teléfono:</label>
                         <input type="text" name="txtTelefono" id="txtTelefono" class="form-control">
                     </div>
                     <div class="p-2">
-                        <label form="">Edad:</label>
+                        <label form="txtEdad">Edad:</label>
                         <input type="text" name="txtEdad" id="txtEdad" class="form-control">
                     </div>
                     <div class="p-2">
-                        <button type="submit" class="btn btn-primary">Enviar</button>
+                        <button type="submit" name="btnEnviar" class="btn btn-primary">Enviar</button>
 
-                        <button type="submit" class="btn btn-danger">Eliminar</button>
+                        <button type="submit" name="btnEliminar" class="btn btn-danger">Eliminar</button>
                     </div>
 
                 </form>
@@ -78,11 +116,12 @@ if ($_POST) {
                             <th>DNI:</th>
                             <th>Teléfono:</th>
                             <th>Edad:</th>
+                            <th>Acciones:</th>
                         </tr>
                     </thead>
                     <tbody>
                         <?php
-                        foreach ($aClientes as $cliente) {
+                        foreach ($aClientes as $pos => $cliente):
                         ?>
 
                             <tr>
@@ -90,9 +129,12 @@ if ($_POST) {
                                 <td><?php echo $cliente["dni"]; ?></td>
                                 <td><?php echo $cliente["telefono"]; ?></td>
                                 <td><?php echo $cliente["edad"]; ?></td>
+                                <td>
+                                    <a href="clientes_session.php?pos=<?php echo $pos; ?>"><i class="bi bi-trash3"></i> </a>
+                                </td>
                             </tr>
 
-                        <?php } ?>
+                        <?php endforeach; ?>
 
                     </tbody>
                 </table>
